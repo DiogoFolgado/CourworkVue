c55baf942168bda4f58adbd403b7b5fb97f11f26
DiogoFolgado <56917556+DiogoFolgado@users.noreply.github.com>
2020-02-07 14:00:40 +0000

    Update server.js

--- course_work/src/server.js
+++ course_work/src/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs"; // encrypt pass 
 import { mongoURL } from '../config'
 import bcryptSaltRounds from '../config'
 
