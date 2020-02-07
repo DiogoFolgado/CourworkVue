520fabfb008b6aa9569692e1968654350c772cfb
DiogoFolgado <56917556+DiogoFolgado@users.noreply.github.com>
2020-02-07 13:40:06 +0000

    Add files via upload

---
+++ course_work/config/index.js
@@ -0,0 +1,5 @@
+export default {
+  bcrypt: { SALT_ROUNDS: 10 },
+}
+
+export const mongoURL = 'mongodb+srv://courseWorkUser:courseWork123@cluster0-vnbai.mongodb.net/test?retryWrites=true&w=majority'
